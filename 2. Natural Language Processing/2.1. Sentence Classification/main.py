4dcf5e22e70f2f8cf96268d0efff9a1158aaef18
Yoon Ki JEONG <yoongi0428@naver.com>
2018-10-13 21:13:11 +0900

    sentence class implemented

--- 2. Natural Language Processing/2.1. Sentence Classification/main.py
+++ 2. Natural Language Processing/2.1. Sentence Classification/main.py
@@ -1,66 +1,86 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from utils.dataset import Dataset
+from utils.data_utils import load_data, load_vocab, cpu, gpu
 from models.model import RNN, CNN
+import numpy as np
 
 def batch_loader(Dataset, n=1):
-    data_len = len(Dataset)
+    x = Dataset[0]
+    y = Dataset[1]
+    data_len = len(x)
 
     for i in range(0, data_len, n):
-        review, rating = Dataset[i: min(data_len, i + n)]
-        yield review, rating
+        yield x[i: min(data_len, i + n), :], y[i: min(data_len, i + n)]
         # yield torch.from_numpy(review).type(torch.LongTensor), torch.from_numpy(rating).type(torch.FloatTensor)
 
+def accuracy(pred, ans):
+    total = len(ans)
 
+    pred = np.concatenate(pred)
+    pred[pred > 0.5] = 1
+    pred[pred <= 0.5] = 0
+
+    same = np.array(pred == ans, dtype=int)
+
+    return float(sum(same)) / total
 
 # Hyperparameters
 ### DATA
-embedding = 128
+embedding = 256
 output_dim = 1
-maxlen = 70
+maxlen = 150
+threshold = 0.5
 eumjeol = True
 
 ### RNN
 rnn_hidden = 256
 num_layers = 1
-bi = False
+bi = True
 
 ### CNN
+filters = [3, 5, 7]
+num_filters = [64, 64, 64]
 
 ### Training
-epochs = 100
+epochs = 10
 batch = 256
-lr = 0.01
-gpu = True
+lr = 0.001
+cuda = True
 
-model = "rnn"   # 'cnn'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Dataset
-training_data = Dataset("data/ratings_train.txt", maxlen, eumjeol)
-vocab_size = training_data.vocab_size()
+model = "cnn"   # 'cnn'
+
+vocabs = load_vocab('data/aclimdb/imdb.vocab')
+w2i = {w: i for i, w in enumerate(vocabs)}
+i2w = {i: w for i, w in enumerate(vocabs)}
+vocab_size = len(vocabs)
+
+train_x, train_y = load_data('data', w2i, maxlen, train=True)
 
 # Model
-model = RNN(embedding, rnn_hidden, num_layers, bi, output_dim, vocab_size) if model == 'rnn' else CNN()
-criterion = nn.CrossEntropyLoss()
+model = RNN(embedding, rnn_hidden, num_layers, bi, output_dim, vocab_size) \
+    if model == 'rnn' else CNN(filters, num_filters, maxlen, vocab_size, embedding, output_dim)
+# criterion = nn.CrossEntropyLoss()
 
-optim = torch.optim.SGD(model.parameters(), lr)
+optim = torch.optim.Adam(model.parameters(), lr)
 
-if gpu:
-    model.cuda()
-    training_data.cuda()
+if cuda:
+    model.to(device)
+    train_x = train_x.cuda()
+    train_y = train_y.cuda()
 
 # Train
 model.train()
 for epoch in range(1, epochs + 1):
     loss = 0.0
 
-    for i, (batch_x, batch_y) in enumerate(batch_loader(training_data, batch)):
+    for i, (batch_x, batch_y) in enumerate(batch_loader((train_x, train_y), batch)):
         optim.zero_grad()
 
         logit = model(batch_x)
 
-        # l = criterion(logit, batch_y)
         l = F.binary_cross_entropy(logit, batch_y)
 
         l.backward()
@@ -70,4 +90,19 @@ for epoch in range(1, epochs + 1):
 
     print('[Epoch %3d] Loss = %.4f' % (epoch, loss))
 
+del train_x, train_y
+
 # Test
+test_x, test_y = load_data('data', w2i, maxlen, train=False)
+if cuda:
+    test_x = test_x.cuda()
+    test_y = test_y.cuda()
+
+pred = []
+for i, (batch_x, batch_y) in enumerate(batch_loader((test_x, test_y), batch)):
+    out = model(batch_x)
+
+    pred.append(out.data.cpu().numpy())
+
+acc = accuracy(pred, test_y)
+print('\nTEST ACCURACY : %.4f' % acc)
--- 2. Natural Language Processing/2.1. Sentence Classification/models/model.py
+++ 2. Natural Language Processing/2.1. Sentence Classification/models/model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class RNN(nn.Module):
     def __init__(self, emb_dim, rnn_hidden, num_layers, bi, output_dim, vocab_size):
@@ -21,7 +21,7 @@ class RNN(nn.Module):
             bidirectional=bi,
             batch_first=True
         )
-        self.out_proj = nn.Linear(rnn_hidden, output_dim)
+        self.out_proj = nn.Linear(rnn_hidden * 2 if bi else rnn_hidden, output_dim)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input):
@@ -35,4 +35,49 @@ class RNN(nn.Module):
         return self.sigmoid(logit)
 
 class CNN(nn.Module):
-    pass
+    def __init__(self, filters, num_filters, maxlen, vocab_size, emb_dim, output_dim):
+        super(CNN, self).__init__()
+
+        self.filters = filters
+        self.num_filters = num_filters
+        self.vocab_size = vocab_size
+        self.emb_dim = emb_dim
+        self.output_dim = output_dim
+
+        self.embedding = nn.Embedding(vocab_size, self.emb_dim)
+
+        self.convs = nn.ModuleList([
+            nn.Conv2d(in_channels=1, out_channels=num, kernel_size=(size, emb_dim), padding=(size//2, 0))
+            for size, num in zip(filters, num_filters)
+        ])
+
+        self.maxpool = nn.ModuleList([
+            nn.MaxPool2d((1, num), stride=1)
+            for num in num_filters
+        ])
+
+        self.out_proj = nn.Linear(maxlen * len(filters), output_dim)
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input):
+        input = self.embedding(input)
+        input = input.unsqueeze(1)
+
+        conv_out = []
+        for conv, mp in zip(self.convs, self.maxpool):
+            out = self.relu(conv(input))
+            out = out.permute(0, 3, 2, 1)
+
+            pooled = mp(out).squeeze()
+
+            conv_out.append(pooled)
+
+        # concat
+        conv_out = torch.cat(conv_out, 1)
+
+        # proj
+        logit = self.out_proj(conv_out)
+
+        return self.sigmoid(logit)
